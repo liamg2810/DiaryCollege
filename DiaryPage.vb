323e03c0889fad2b9893d3e7b61534ccf81944f3
lg05264093 <lg05264093@LC21205XX>
2024-10-03 14:53:59 +0100

    asd

--- DiaryPage.vb
+++ DiaryPage.vb
@@ -1,4 +1,15 @@
-﻿Public Class DiaryPage
+﻿Imports System.IO
+Imports Newtonsoft.Json
+
+Public Class DiaryPage
+
+    Private Function GetJsonFilePath() As String
+        Dim baseDir As String = AppDomain.CurrentDomain.BaseDirectory
+
+        Dim projectRoot As String = Path.Combine(baseDir, "..", "..", "Data", "data.json")
+
+        Return Path.GetFullPath(projectRoot)
+    End Function
 
     Private Sub DiaryPage_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
         Application.Exit()
@@ -21,6 +32,29 @@
             Return
         End If
 
-        Dim data As New Dictionary(Of
+        Dim data As New Dictionary(Of String, Object) From {
+            {"title", titleVal},
+            {"date", dateVal},
+            {"entry", entry},
+            {"mood", mood},
+            {"targets", targets}
+        }
+
+        Dim jsonData As String = File.ReadAllText(GetJsonFilePath())
+
+        Dim diaryEntries As List(Of Dictionary(Of String, Object))
+
+        If String.IsNullOrWhiteSpace(jsonData) Then
+            diaryEntries = New List(Of Dictionary(Of String, Object))()
+        Else
+            diaryEntries = JsonConvert.DeserializeObject(Of List(Of Dictionary(Of String, Object)))(jsonData)
+        End If
+
+        diaryEntries.Add(data)
+
+        Dim updatedJson As String = JsonConvert.SerializeObject(diaryEntries, Formatting.Indented)
+
+        File.WriteAllText(GetJsonFilePath(), updatedJson)
+
     End Sub
 End Class
